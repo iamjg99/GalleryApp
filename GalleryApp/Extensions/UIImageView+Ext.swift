78b6f9567a35de3498922d97d5ff36c61b572d05
jatincopper <jatin.g@coppermobile.com>
2023-11-26 05:16:35 +0530

    done

---
+++ GalleryApp/Extensions/UIImageView+Ext.swift
@@ -0,0 +1,17 @@
+//
+//  UIButton+Ext.swift
+//  GalleryApp
+//
+//  Created by Jatin on 26/11/23.
+//
+
+import Foundation
+import UIKit
+
+extension UIImageView {
+    
+    func circularView() {
+        layer.cornerRadius = min(bounds.width, bounds.height) / 2.0
+        layer.masksToBounds = true
+    }
+}
